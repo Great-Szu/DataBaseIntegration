09a9b2df3cb377b55c06ad4376574524ae397776
Great-Szu <mateusz.chol.net@outlook.com>
2025-09-22 19:38:51 +0200

    Day 13 creating subclass from tkinter class, connecting records from database with tkinter

--- star_args.py
+++ star_args.py
@@ -1,7 +1,23 @@
-def print_backwards(*args, **kwargs):
+# def print_backwards(*args, **kwargs):
+#     print(kwargs)
+#     for word in args[::-1]:
+#         print(word[::-1], **kwargs)
 
-    for word in args[::-1]:
-        print(word[::-1], **kwargs)
+def print_backwards(*args, **kwargs):
+    end_character = kwargs.pop('end', '\n')
+    sep_character = kwargs.pop('sep', ' ')
+    for word in args[:0:-1]:
+        print(word[::-1], end=sep_character, **kwargs)
+    print(args[0][::-1], end=end_character, **kwargs)
 
 with open("backwards.txt", 'w') as backwards:
-    print_backwards("hello", "test", "take", end=' ', file=backwards)
+    print_backwards("hello", "test", "take", end='\t')
+    print("Another String")
+
+
+print()
+print("hello", "test", "take", end='', sep='\n**\n')
+print_backwards("hello", "test", "take", end='', sep='\n**\n')
+print("!")
+print()
+print("test")
